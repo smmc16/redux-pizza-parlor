5081067ad05e31923e462cd841ef96575373cdce
Sarah McCartney <smmc16@gmail.com>
2024-04-14 19:00:48 -0500

    addeduseEffect and function to contain the get request

--- src/components/DisplayPizzas/DisplayPizzas.jsx
+++ src/components/DisplayPizzas/DisplayPizzas.jsx
@@ -4,10 +4,20 @@ import { useState, useEffect } from 'react';
 function DisplayPizzas() {
     const [pizzaList, setPizzaList] = useState([]);
 
+    function getPizzas() {
     axios.get('/api/pizza').then((response) => {
         setPizzaList(response.data);
     }).catch(error => {
         console.log(error);
         alert('Something went wrong!');
-    })
+    });
+    }
+
+    useEffect(() => {
+        getPizzas();
+    }, [])
+
+    return (
+        <></>
+    )
 }
