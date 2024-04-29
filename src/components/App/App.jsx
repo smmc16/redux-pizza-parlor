c26eaeeffdb1423e01d25c3066c6cec88bb4b072
Sarah McCartney <smmc16@gmail.com>
2024-04-28 19:10:40 -0500

    added checkout and order total

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,18 +3,27 @@ import axios from 'axios';
 import './App.css';
 import DisplayPizzas from '../DisplayPizzas/DisplayPizzas.jsx';
 import CustomerInfoForm from '../CustomerInfoForm/CustomerInfoForm.jsx';
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link, useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function App() {
+const pizza = useSelector(store => store.sendOrderInfo);
+const history = useHistory();
+
+function checkout () {
+    history.push('/info');
+}
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
+      <br />
+        <p>Order total:{pizza.price}</p>
+        <button onClick={checkout}>Checkout</button>
 
       
       <Router>
