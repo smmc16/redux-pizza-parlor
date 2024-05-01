a3e58ad22a641dd68677b9fc5699496633a65ce9
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-05-01 09:51:13 -0500

    added styling to each page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,43 +1,54 @@
 import React from 'react';
-import axios from 'axios';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
 import DisplayPizzas from '../DisplayPizzas/DisplayPizzas.jsx';
 import CustomerInfoForm from '../CustomerInfoForm/CustomerInfoForm.jsx';
-import { HashRouter as Router, Route, Link, useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import Checkout from '../Checkout/Checkout.jsx';
 import Admin from '../Admin/Admin.jsx';
+import { useSelector } from 'react-redux';
 
 function App() {
-const pizza = useSelector(store => store.sendOrderInfo);
-let total = pizza.price;
+  const pizza = useSelector(store => store.sendOrderInfo);
+  let total = pizza.price;
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-      <br />
-        <p>Order total:{total}</p>
-
-      
-      <Router>
-        <Route exact path="/">
-          <DisplayPizzas />
-        </Route>
-        <Route path="/info">
-          <CustomerInfoForm />
-        </Route>
-        <Route path ="/checkout">
-          <Checkout />
-        </Route>
-        <Route path ="/admin">
-          <Admin />
-        </Route>
-      </Router>
-    </div>
+    <Router>
+      <div>
+        <div className='navbar'>
+          <div className='nav-left'>
+            <NavLink to="/" activeClassName="active" exact>
+              Home
+            </NavLink>
+            {/* <NavLink to="/info" activeClassName="active">
+              Customer Info
+            </NavLink>
+            <NavLink to="/checkout" activeClassName="active">
+              Checkout
+            </NavLink> */}
+            <NavLink to="/admin" activeClassName="active">
+              Admin
+            </NavLink>
+          </div>
+          <div className="nav-right">
+            <div className="total">
+              <p>Order Total: {total}</p>
+            </div>
+          </div>
+        </div>
+        <div className='content-bar'>
+          <div className='content'>
+            <header className='App-header'>
+              <h1 className='App-title'>Prime Pizza</h1>
+            </header>
+            <p className='App-para'>Pizza is great.</p>
+            <Route exact path="/" component={DisplayPizzas} />
+            <Route path="/info" component={CustomerInfoForm} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/admin" component={Admin} />
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/DisplayPizzas/DisplayPizzas.jsx
+++ src/components/DisplayPizzas/DisplayPizzas.jsx
@@ -3,60 +3,78 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './DisplayPizzas.css';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 function DisplayPizzas() {
-    
     const dispatch = useDispatch();
     const history = useHistory();
-    
 
     const [pizzaList, setPizzaList] = useState([]);
 
     function getPizzas() {
-    axios.get('/api/pizza').then((response) => {
-        setPizzaList(response.data);
-    }).catch(error => {
-        console.log(error);
-        alert('Something went wrong!');
-    });
+        axios.get('/api/pizza')
+            .then((response) => {
+                setPizzaList(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+                alert('Something went wrong!');
+            });
     }
 
     useEffect(() => {
         getPizzas();
-    }, [])
+    }, []);
 
     const removePizza = (id) => {
         console.log(id);
-        let action = {type: 'CLEAR_CART'}
+        let action = { type: 'CLEAR_CART' }
         dispatch(action);
     }
 
-
-    function addPizza (pizza) {
-        let action = {type: 'SEND_PIZZA', payload: {id: pizza.id, name: pizza.name, price: pizza.price}}
+    function addPizza(pizza) {
+        let action = { type: 'SEND_PIZZA', payload: { id: pizza.id, name: pizza.name, price: pizza.price } }
         dispatch(action);
     }
 
-    function checkout () {
+    function checkout() {
         history.push('/info');
     }
 
     return (
         <>
-        <button onClick={checkout}>Checkout</button>
-        <div className='list'>
-        {pizzaList.map(pizza => (
-            <div className='pizza' key={pizza.id}>
-            <h3>{pizza.name + ' ' + pizza.price}</h3>
-            <br />
-            <img src={pizza.image_path} />
-            <p>{pizza.description}</p>
-            <button onClick={() => addPizza(pizza)}>Add to cart</button> 
-            <button onClick={() => removePizza(pizza.id)}>Remove</button>
-            
+            <div className="checkout-button">
+                <Button onClick={checkout} variant="contained" color="primary" size="small">Click to Checkout</Button>
             </div>
-        ))}
-        </div>
+            <div className='list'>
+                {pizzaList.map(pizza => (
+                    <Card key={pizza.id} className='pizza'>
+                        <CardMedia
+                            component="img"
+                            height="200"
+                            image={pizza.image_path}
+                            alt={pizza.name}
+                        />
+                        <CardContent className="pizzaContent">
+                            <Typography gutterBottom variant="h5" component="div">
+                                {pizza.name}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                {pizza.description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions className="actions">
+                            <Button variant="contained" color="success" size="small" onClick={() => addPizza(pizza)}>Add to cart</Button>
+                            <Button variant="contained" color="error" size="small" onClick={() => removePizza(pizza.id)}>Remove</Button>
+                        </CardActions>
+                    </Card>
+                ))}
+            </div>
         </>
     )
 }
